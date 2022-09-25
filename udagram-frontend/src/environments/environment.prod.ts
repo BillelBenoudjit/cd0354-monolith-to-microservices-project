84f17b9fb63fdb78233047c91a07bae29f5eac70
BillelBenoudjit <hb_benoudjit@esi.dz>
2022-09-25 17:37:57 +0200

    Change environment variables

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a113dd704cf2a49a5991d57a93dabc42-846196458.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
